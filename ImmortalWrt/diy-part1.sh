e41374194fcea3b22dab5e62b40ac3d7622a0f1f
txtyb <txtybbill@163.com>
2024-03-20 16:09:34 +0800

    ax6000: fix conflicts of feeds sources

--- ImmortalWrt/diy-part1.sh
+++ ImmortalWrt/diy-part1.sh
@@ -4,3 +4,7 @@
 # sed -i '$a src-git freifunk https://github.com/freifunk/openwrt-packages.git' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
