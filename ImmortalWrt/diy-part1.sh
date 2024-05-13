8e49fee40551c2bc40de7c1650d1d59bdf98064c
txtyb <txtybbill@163.com>
2024-05-13 17:23:37 +0800

    ax6000: remove shadowsocks-libev from immortalwrt packages feed

--- ImmortalWrt/diy-part1.sh
+++ ImmortalWrt/diy-part1.sh
@@ -6,5 +6,6 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns,mosdns}
+rm -rf feeds/packages/net/shadowsocks-libev
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
