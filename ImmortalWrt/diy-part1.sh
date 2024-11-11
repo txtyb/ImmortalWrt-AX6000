f01e95890bef1ad72e054bf27853a8aa90a56a5f
txtyb <txtybbill@163.com>
2024-11-11 14:50:11 +0800

    Revert "ax6000: update gcc tool chain"
    This reverts commit c696ba89c0b2e1ac284d088525baf52ae39e6750.

--- ImmortalWrt/diy-part1.sh
+++ ImmortalWrt/diy-part1.sh
@@ -7,22 +7,3 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns,mosdns}
 rm -rf feeds/packages/net/shadowsocks-libev
-
-# 添加PPA源
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt update
-
-# 安装最新的GCC（假设为GCC 11）
-sudo apt install gcc-11 g++-11
-
-# 设置默认版本
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 100
-
-# 配置默认版本
-sudo update-alternatives --config gcc
-sudo update-alternatives --config g++
-
-# 验证安装
-gcc --version
-g++ --version
