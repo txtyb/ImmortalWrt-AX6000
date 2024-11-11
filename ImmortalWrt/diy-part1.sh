18f6919aa6892973b407cd2077749d154b9385b9
txtyb <txtybbill@163.com>
2024-11-11 14:50:41 +0800

    Revert "ax6000: reveal original golang"
    This reverts commit eb1da54f686f7ec069abf39476bea1617fcedf78.

--- ImmortalWrt/diy-part1.sh
+++ ImmortalWrt/diy-part1.sh
@@ -7,3 +7,5 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns,mosdns}
 rm -rf feeds/packages/net/shadowsocks-libev
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
